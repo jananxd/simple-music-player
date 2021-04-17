45c8c7d1d7efda432e456800d9793c709f4a91c1
John Carlo A. Miel <johncarlomiel@gmail.com>
2021-04-17 14:55:37 +0800

    :sparkles: added router with root page

---
+++ src/pages/Library.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Library: React.FC<{}> = () => {
+
+    return (
+        <Text>
+            Library
+        </Text>
+    );
+}
+
+export default Library;
